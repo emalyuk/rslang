8945a6e49deeea0e0dd7dcc104aecaef215f8d82
anreikas <kazeyandrey@gmail.com>
2020-06-21 22:10:11 +0300

    refactor: transfer data and devide by files

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+const imgPath = '/src/assets/img/';
+const githubUserLink = 'https://github.com/';
+
+
+
+
+export { imgPath, githubUserLink };
---
+++ src/constants/teamData.js
@@ -0,0 +1,67 @@
+const teamData = [
+  {
+    github: 'TopCoder3000',
+    name: 'Eugene Malyuk',
+    position: 'Project Manager',
+    city: 'Minsk',
+    avatar: 'eMalyuk.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'abukato',
+    name: 'Artur Bukato',
+    position: 'Frontend Developer',
+    city: 'Minsk',
+    avatar: 'aBukato.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'anreikas',
+    name: 'Andrey Kazey',
+    position: 'Frontend Developer',
+    city: 'Minsk',
+    avatar: 'aKazey.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'himimetsu',
+    name: 'Vladislav Ivanov',
+    position: 'Frontend Developer',
+    city: 'Nizhny Novgorod',
+    avatar: 'vIvanov.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'krohnic',
+    name: 'Nickolay Kroh',
+    position: 'Frontend Developer',
+    city: 'Minsk',
+    avatar: 'nKroh.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'a-klimovich',
+    name: 'Arthur Klimovich',
+    position: 'Frontend Developer',
+    city: 'Lida',
+    avatar: 'aKlimovich.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+  {
+    github: 'aleksey-drozdov',
+    name: 'Aleksey Drozdov',
+    position: 'Frontend Developer',
+    city: 'Minsk',
+    avatar: 'aDrozdov.jpg',
+    contribution:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.',
+  },
+];
+
+export default teamData;
