454de7db7120cbc9e48f222f709ff0e13bb3d548
Lesha Drozdov <leshadrozdov@gmail.com>
2020-06-28 17:03:53 +0300

    Fix: img src, add UseState

--- src/pages/promo/components/faq/Faq.js
+++ src/pages/promo/components/faq/Faq.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Collapse from '@kunukn/react-collapse';
 import './Faq.scss';
 
-export const Faq = ({ question, answer }) => {
+export const Faq = ({ question, answer, id, activeId, setActiveId }) => {
+  const isActive = activeId === id;
+
   return (
-    <div className='faqItem'>
-      <div className='faqItem-title'>
+    <div className={['faqItem', isActive ? 'isActive' : ''].join(' ')}>
+      <div className='faqItem-title' onClick={() => setActiveId(id)}>
         <div className='faqItem-openBtn' />
         <p className='faqItem-question'>{question}</p>
       </div>
-      <div className='faqItem-answer'>
+      <Collapse isOpen={isActive} className='faqItem-answer'>
         <p>{answer}</p>
-      </div>
+      </Collapse>
     </div>
   );
 };
