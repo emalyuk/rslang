da230aa4d40adeea3ca0b95fd374bac93432e280
Lesha Drozdov <leshadrozdov@gmail.com>
2020-06-28 17:03:53 +0300

    Fix: img src, add UseState

--- src/pages/promo/Promo.js
+++ src/pages/promo/Promo.js
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Promo.scss';
-import data from '../../constants/promoData.json'
-import Service from './components/service/Service'
-import Feature from './components/feature/Feature'
-import Faq from './components/faq/Faq'
+import data from '../../constants/promoData';
+import Service from './components/service/Service';
+import Feature from './components/feature/Feature';
+import Faq from './components/faq/Faq';
+import image from '../../assets/images/promo/hero-mobile.png';
 
 export const Promo = () => {
-  function openFaq(e) {
-    const faqArr = Array.from(e.target.parentElement.parentElement.childNodes)
-    const faqItem = e.target.parentElement;
-    if (faqItem.classList.contains('isActive')) {
-      faqItem.classList.remove('isActive')
-    } else {
-      faqArr.map((item) => {
-        item.classList.remove('isActive')
-      });
-      faqItem.classList.add('isActive');
-    }
-  }
+  const [activeId, setActiveId] = useState(false);
 
   return (
     <div className='Promo'>
-
       <section className='hero'>
         <div className='container hero-section'>
           <div className='hero-description'>
             <h1>The Best App in the Universe</h1>
             <p>
               RS Lang - это эффективный сервис для нескучного изучения
-              английского языка. Более 6 человек во всем мире
-              выбрали RS Lang.
+              английского языка. Более 6 человек во всем мире выбрали RS Lang.
             </p>
           </div>
           <div className='hero-image'>
-            <img src='./assets/images/promo/hero-mobile.png' alt='mobile' />
+            <img src={image} alt='mobile' />
           </div>
         </div>
       </section>
@@ -41,12 +29,7 @@ export const Promo = () => {
       <section className='services-section'>
         <div className='services container'>
           {data.service.map((item) => (
-            <Service
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
-              key={item.id}
-            />
+            <Service key={item.id} {...item} />
           ))}
         </div>
       </section>
@@ -54,17 +37,12 @@ export const Promo = () => {
       <section className='features container'>
         <h2 className='title'>ОСОБЫЕ ВОЗМОЖНОСТИ</h2>
         <p className='features-description'>
-          Наша команда разработчиков провела опросы и постаралась сделать
-          для Ваc максимально удобное приложение для изучения языка.
+          Наша команда разработчиков провела опросы и постаралась сделать для
+          Ваc максимально удобное приложение для изучения языка.
         </p>
         <div className='features-items'>
           {data.feature.map((item) => (
-            <Feature
-              icon={item.icon}
-              title={item.title}
-              description={item.description}
-              key={item.id}
-            />
+            <Feature key={item.id} {...item} />
           ))}
         </div>
       </section>
@@ -72,14 +50,14 @@ export const Promo = () => {
       <section className='faq'>
         <div className='faq-content container'>
           <h2 className='faq-title'>ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h2>
-          <div
-            className='faq-items'
-            onClick={(e) => {
-              openFaq(e)
-            }}
-          >
+          <div className='faq-items'>
             {data.faq.map((item) => (
-              <Faq question={item.question} answer={item.answer} key={item.id} />
+              <Faq
+                {...item}
+                activeId={activeId}
+                setActiveId={setActiveId}
+                key={item.id}
+              />
             ))}
           </div>
         </div>
