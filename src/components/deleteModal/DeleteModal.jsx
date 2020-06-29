5718646c35a7b9350ba2ec394f8b764c248bbfef
Vladislav Ivanov <himametsu@gmail.com>
2020-06-29 22:17:57 +0300

    feature/implement-delete-modal-component

---
+++ src/components/deleteModal/DeleteModal.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './DeleteModal.scss';
+
+const DeleteModal = ({ hideFunc }) => {
+  const deleteWord = () => {
+    console.log('Слова удалены');
+    hideFunc();
+  };
+
+  const clearTrashWords = () => {
+    console.log('Отмена');
+    hideFunc();
+  };
+
+  return (
+    <div className='delete-wrapper'>
+      <div className='delete-window'>
+        <div className='delete-title'>
+          Вы точно хотите удалить эти слова из вашего словаря?
+        </div>
+        <div className='delete-buttons'>
+          <div className='btn-wrapper'>
+            <button
+              className='delete-btn'
+              type='button'
+              onClick={() => deleteWord()}
+            >
+              <span className='delete-btn-content'>
+                Удалить слова
+              </span>
+            </button>
+          </div>
+          <div className='btn-wrapper'>
+            <button
+              className='cancel-btn'
+              type='button'
+              onClick={() => clearTrashWords()}
+            >
+              <span className='cancel-btn-content'>
+                Отмена
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteModal;
