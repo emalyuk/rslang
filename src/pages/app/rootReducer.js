53e31c4aee1d58f82c10c54dfe32d79a98253cf1
Vladislav Ivanov <himametsu@gmail.com>
2020-06-29 21:45:59 +0300

    refactor/connect-dictionary-reducer

--- src/pages/app/rootReducer.js
+++ src/pages/app/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { loginSliceReducer } from 'pages/auth/login/LoginSliceReducer';
+import { dictionarySliceReducer } from '../dictionary/DictionaryReducer';
 
 export const rootReducer = combineReducers({
   login: loginSliceReducer,
+  dictionary: dictionarySliceReducer,
 });
 
 export default rootReducer;
