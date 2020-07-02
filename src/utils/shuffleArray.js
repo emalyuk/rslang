aca92dd23d75e6e5f7cf3acd9331fa4fe9356970
Vladislav Ivanov <himametsu@gmail.com>
2020-07-02 19:36:34 +0300

    feature/implement-shuffle-array

---
+++ src/utils/shuffleArray.js
@@ -0,0 +1,14 @@
+const shuffleArray = (paramArray) => {
+  const arr = paramArray
+  let j;
+  let temp;
+  for (let i = arr.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+};
+
+export default shuffleArray;
