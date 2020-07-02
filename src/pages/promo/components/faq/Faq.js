0f4a9ad0a1d38340b11e3d8e13126c0b1845cd11
Lesha Drozdov <leshadrozdov@gmail.com>
2020-07-02 20:10:08 +0300

    fix/check-for-open-item-added

--- src/pages/promo/components/faq/Faq.js
+++ src/pages/promo/components/faq/Faq.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Collapse from '@kunukn/react-collapse';
 import './Faq.scss';
 
-export const Faq = ({ question, answer, id, activeId, setActiveId }) => {
+export const Faq = ({
+  question, answer, id, activeId, setActiveId,
+}) => {
   const isActive = activeId === id;
 
+  function checkIsActive(id) {
+    activeId === id ? setActiveId('') : setActiveId(id)
+  }
   return (
     <div className={['faqItem', isActive ? 'isActive' : ''].join(' ')}>
-      <div className='faqItem-title' onClick={() => setActiveId(id)}>
+      <div className='faqItem-title' onClick={() => checkIsActive(id)}>
         <div className='faqItem-openBtn' />
         <p className='faqItem-question'>{question}</p>
       </div>
