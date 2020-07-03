0304d9a2944dfb0d290dd02a0fddc981ff20663a
Artur Bukato <artur.bukato@gmail.com>
2020-07-03 17:39:03 +0300

    feature/add-check-switch-option-possibility

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,83 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getHomeInfo } from './HomeSliceReducer';
 
 import HomeStatus from './components/homeStatus/HomeStatus';
 import CardSettings from './components/cardSettings/CardSettings';
 
+import { initialSettings, settingsLabelName } from '../../constants/cardSettingsData';
+
 import './Home.scss';
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { data, errors } = useSelector((state) => state.home);
-
+  
+  // TODO: get server info, useCallback, useMemo
   // MockData
   const wordsCountLearned = 555;
-  const totalCardsPerDay = 20;
+
+  const [settings, setSettings] = useState(initialSettings);
+
+  const handleOnChangeSelect = (event, tag) => {
+    const selectedValue = Number(event.target.value);
+
+    if (tag === settingsLabelName.maxCardsPerDay.en) {
+      setSettings({
+        ...settings,
+        optional: {
+          ...settings.optional,
+          newCardsPerDay: selectedValue,
+        },
+        wordsPerDay: selectedValue,
+      });
+    } else {
+      setSettings({
+        ...settings,
+        optional: {
+          ...settings.optional,
+          newCardsPerDay: selectedValue,
+        },
+      });
+    }
+  }
+
+  const handleOnChangeSwitcher = (option, categoryLabel) => {
+    const index = settings.optional[categoryLabel].findIndex((el) => el.option === option);
+
+    const oldItem = settings.optional[categoryLabel][index];
+    const newItem = {
+      ...oldItem,
+      isChecked: !oldItem.isChecked,
+    };
+
+    const newSettings = [
+      ...settings.optional[categoryLabel].slice(0, index),
+      newItem,
+      ...settings.optional[categoryLabel].slice(index + 1),
+    ];
+
+    if (categoryLabel === settingsLabelName.mainInfoOnCard.en) {
+      if (newSettings.every((el) => !el.isChecked)) {
+        return false;
+      }
+    }
+
+    setSettings({
+      ...settings,
+      optional: {
+        ...settings.optional,
+        [categoryLabel]: newSettings,
+      },
+    });
+  }
+
+  useEffect(() => {
+    localStorage.setItem('settings', JSON.stringify(settings));
+    // return () => {
+    //   cleanup
+    // }
+  }, [settings])
 
   useEffect(() => {
     dispatch(getHomeInfo());
@@ -24,9 +88,13 @@ export const Home = () => {
       <div className='home-container container'>
         <HomeStatus
           wordsCountLearned={wordsCountLearned}
-          totalCardsPerDay={totalCardsPerDay}
+          totalCardsPerDay={settings.wordsPerDay}
         />
-        <CardSettings />
+        <CardSettings
+          settings={settings}
+          handleOnChangeSelect={handleOnChangeSelect}
+          handleOnChangeSwitcher={handleOnChangeSwitcher}
+        />
       </div>
     </div>
   );
--- src/pages/home/components/cardSettings/CardSettings.jsx
+++ src/pages/home/components/cardSettings/CardSettings.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import CardSettingsSwitcherBox from '../cardSettingsSwitcherBox/CardSettingsSwitcherBox';
 import CardsCountSelect from '../cardsCountSelect/CardsCountSelect';
@@ -9,13 +10,11 @@ import {
   minLimitNewCardsPerDay,
 } from '../../../../constants/constants';
 import arrayOfNumbers from '../../../../utils/arrayOfNumber';
-import { initialSettings, settingsLabelName } from '../../../../constants/cardSettingsData';
+import { settingsLabelName } from '../../../../constants/cardSettingsData';
 
 import './CardSettings.scss';
 
-const CardSettings = () => {
-  const [settings, setSettings] = useState(initialSettings);
-
+const CardSettings = ({ settings, handleOnChangeSelect, handleOnChangeSwitcher }) => {
   const maxCardsArrayOfNumber = arrayOfNumbers(
     minLimitCardsPerDay,
     maxLimitCardsPerDay - minLimitCardsPerDay + 1,
@@ -24,61 +23,6 @@ const CardSettings = () => {
   const maxNewCardsArrayOfNumber = arrayOfNumbers(
     minLimitNewCardsPerDay, settings.wordsPerDay + 1,
   );
-
-  const handleOnChangeSelect = (event, tag) => {
-    const selectedValue = Number(event.target.value);
-
-    if (tag === settingsLabelName.maxCardsPerDay.en) {
-      setSettings({
-        ...settings,
-        optional: {
-          ...settings.optional,
-          newCardsPerDay: selectedValue,
-        },
-        wordsPerDay: selectedValue,
-      });
-    } else {
-      setSettings({
-        ...settings,
-        optional: {
-          ...settings.optional,
-          newCardsPerDay: selectedValue,
-        },
-      });
-    }
-  }
-
-  // TODO: useCallback, useMemo ? Check switch possibility
-  const handleOnChangeSwitcher = (option, categoryLabel) => {
-    const index = settings.optional[categoryLabel].findIndex((el) => el.option === option);
-
-    const oldItem = settings.optional[categoryLabel][index];
-    const newItem = {
-      ...oldItem,
-      isChecked: !oldItem.isChecked,
-    };
-
-    const newSettings = [
-      ...settings.optional[categoryLabel].slice(0, index),
-      newItem,
-      ...settings.optional[categoryLabel].slice(index + 1),
-    ];
-
-    setSettings({
-      ...settings,
-      optional: {
-        ...settings.optional,
-        [categoryLabel]: newSettings,
-      },
-    });
-  }
-
-  useEffect(() => {
-    localStorage.setItem('settings', JSON.stringify(settings));
-    // return () => {
-    //   cleanup
-    // }
-  }, [settings])
 
   return (
     <div className='home__settings home-box'>
@@ -125,6 +69,15 @@ const CardSettings = () => {
       </div>
     </div>
   );
+};
+
+CardSettings.propTypes = {
+  handleOnChangeSelect: PropTypes.func.isRequired,
+  handleOnChangeSwitcher: PropTypes.func.isRequired,
+  settings: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.object,
+  ]).isRequired,
 };
 
 export default CardSettings;
