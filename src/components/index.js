a2e143e4e99fca89fca1f82b0f02ea00b5331516
Vladislav Ivanov <himametsu@gmail.com>
2020-07-03 21:36:53 +0300

    feature/add-default-import

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './header/Header';
 export { default as TeamCard } from './teamCard/TeamCard';
 export { default as Footer } from './footer/Footer';
 export { default as CheckBox } from './checkBox/CheckBox';
+export { default as DeleteModal } from './deleteModal/DeleteModal';
