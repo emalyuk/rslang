43b8a260e63404b9e9bb9273d84d2c0b746c9595
Vladislav Ivanov <60613410+himimetsu@users.noreply.github.com>
2020-07-06 20:10:34 +0300

    Implement checkbox (#13)
    * feature/implement-check-box-component
    
    * feature/implement-styles-for-check-box-component
    
    * refactor/delete-commented-code
    
    * refactor/add-prop-types-for-check-box-component
    
    * feature/add-default-import
    
    * refactor/update-order
    
    * refactor/rename-prop
    
    * refactor/rename-prop
    
    * refactor/create-data-file

---
+++ src/components/checkBox/CheckBox.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { bgCheckBox } from './data';
+import './CheckBox.scss';
+
+const CheckBox = ({ id, onClick, isChecked }) => {
+  const boxRef = useRef();
+  const iconRef = useRef();
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleCheckBox = () => {
+    if (isActive) {
+      boxRef.current.dataset.active = 'false';
+      iconRef.current.dataset.active = 'false';
+      setIsActive(false);
+      onClick(id);
+    } else {
+      boxRef.current.dataset.active = 'true';
+      iconRef.current.dataset.active = 'true';
+      setIsActive(true);
+      onClick(id);
+    }
+  };
+
+  useEffect(() => {
+    if (isChecked) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }, [isChecked]);
+
+  return (
+    <div className='checkbox-wrapper'>
+      <div className='checkbox'>
+        <label htmlFor={id} className='checkbox_label'>
+          <input className='checkbox_input' type='checkbox' id={id} />
+          <span
+            className='checkbox_box'
+            ref={boxRef}
+            data-active={isChecked ? 'true' : 'false'}
+            onClick={() => toggleCheckBox()}
+          />
+          <span
+            className='checkbox_icon'
+            style={{ backgroundImage: `url(${bgCheckBox})` }}
+            ref={iconRef}
+            data-active={isChecked ? 'true' : 'false'}
+            onClick={() => toggleCheckBox()}
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+CheckBox.propTypes = {
+  id: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  isChecked: PropTypes.bool.isRequired,
+};
+
+export default CheckBox;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Loading } from './loading/Loading';
 export { default as Header } from './header/Header';
 export { default as TeamCard } from './teamCard/TeamCard';
 export { default as Footer } from './footer/Footer';
+export { default as CheckBox } from './checkBox/CheckBox';
