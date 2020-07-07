e115e6cf84d5ca7ea2c12fade5ce6b84ba78db11
Vladislav Ivanov <himametsu@gmail.com>
2020-07-07 21:02:10 +0300

    feature/implement-game-component

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -6,7 +6,7 @@ import GameCard from './gameCard/GameCard';
 const Game = () => {
   return (
     <div className='game-wrapper'>
-      {games.map((game) => <GameCard {...game} key={game.name} />)}
+      {games.map((game, index) => <GameCard {...game} key={game.name} id={index} />)}
     </div>
   );
 };
