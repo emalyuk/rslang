c91cdb30902187b6eba4d55f83424ca69f3a13ea
anreikas <kazeyandrey@gmail.com>
2020-07-12 19:41:17 +0300

    featrue/add-paintings

---
+++ src/constants/paintingsData/index.js
@@ -0,0 +1,17 @@
+import { default as firstGallery } from './level1';
+import { default as secondGallery } from './level2';
+import { default as thirdGallery } from './level3';
+import { default as fourthGallery } from './level4';
+import { default as fifthGallery } from './level5';
+import { default as sixthGallery } from './level6';
+
+const gallery = [
+  firstGallery,
+  secondGallery,
+  thirdGallery,
+  fourthGallery,
+  fifthGallery,
+  sixthGallery,
+];
+
+export gallery;
