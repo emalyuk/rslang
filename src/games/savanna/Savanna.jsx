13e755f96f023a254c5cd2195b17c1271244954c
Vladislav Ivanov <himametsu@gmail.com>
2020-07-14 20:15:39 +0300

    feature/add-authorization-validation

--- src/games/savanna/Savanna.jsx
+++ src/games/savanna/Savanna.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './Savanna.scss';
 import { useSelector, useDispatch } from 'react-redux';
+import Axios from 'axios';
+import { getUserWordsWithFilter } from 'pages/home/HomeApi';
 import { ModalWindow } from '../../components';
 import { defaultHearts } from './data';
-import Axios from 'axios';
-import { getSavannaInfo, changeWordNumber, changeShowCloseModal, changeShowResultsModal, changeWords, changePlaySound, changeIsRefresh } from './SavannaReducer';
+import { getSavannaInfo, changeWordNumber, changeShowCloseModal, changeShowResultsModal, changeWords, changePlaySound, changeIsRefresh, changeShowChangeDifficulity, changeUserWeakWords, changePage } from './SavannaReducer';
 import StartModal from './modals/start/StartModal';
 import CloseModal from './modals/close/CloseModal';
 import ResultsModal from './modals/results/ResultsModal';
@@ -19,8 +20,11 @@ const Savanna = () => {
     wordNumber,
     showStart,
     words,
+    userWeakWords,
+    difficulity,
+    page,
   } = useSelector((state) => state.savanna);
-  const [defaultPage, setDefaultPage] = useState(0);
+  const { isUserLoggedIn } = useSelector((state) => state.login);
   const dispatch = useDispatch();
   const [answerRefs] = useState({
     0: useRef(),
@@ -187,17 +191,30 @@ const Savanna = () => {
   };
 
   const getNewDefaultWords = async () => {
-    const response = await Axios.get(`https://afternoon-falls-25894.herokuapp.com/words?page=${defaultPage + 1}&group=0`);
-    setDefaultPage(defaultPage + 1);
-    const newWords = response.data.map((res) => {
-      return {
-        word: res.word,
-        translate: res.wordTranslate,
-        id: res.id,
-        audio: res.audio,
-      };
-    });
-    return newWords;
+    if (userWeakWords > 19) {
+      const response = await getUserWordsWithFilter('weak');
+      const newWords = response.data[0].paginatedResults.map((res) => {
+        return {
+          word: res.word,
+          translate: res.wordTranslate,
+          id: res.id,
+          audio: res.audio,
+        };
+      });
+      return newWords;
+    } else {
+      const response = await Axios.get(`https://afternoon-falls-25894.herokuapp.com/words?page=${page + 1}&group=${difficulity}`);
+      dispatch(changePage(page + 1));
+      const newWords = response.data.map((res) => {
+        return {
+          word: res.word,
+          translate: res.wordTranslate,
+          id: res.id,
+          audio: res.audio,
+        };
+      });
+      return newWords;
+    }
   };
 
   const updateGame = async () => {
@@ -206,12 +223,45 @@ const Savanna = () => {
       invalid: [],
     });
     answerTrueRef.current.dataset.state = 'start';
-    setHearts(defaultHearts);
+    const copyDefaultHearts = [
+      {
+        id: 1,
+        live: true,
+      },
+      {
+        id: 2,
+        live: true,
+      },
+      {
+        id: 3,
+        live: true,
+      },
+      {
+        id: 4,
+        live: true,
+      },
+      {
+        id: 5,
+        live: true,
+      },
+    ];
+    setHearts(copyDefaultHearts);
     setFails(0);
     const newWords = await getNewDefaultWords();
     dispatch(changeWords(newWords))
     dispatch(changeWordNumber(0));
     dispatch(changeIsRefresh(false));
+  };
+
+  const getUserWordsWrapper = async () => {
+    const studiedWords = await getUserWordsWithFilter('weak');
+    dispatch(changeUserWeakWords(studiedWords.data[0].paginatedResults.length));
+    if (studiedWords.data[0].paginatedResults.length > 19) {
+      dispatch(getSavannaInfo(true));
+    } else {
+      dispatch(getSavannaInfo(false));
+      dispatch(changeShowChangeDifficulity(true));
+    }
   };
 
   useEffect(() => {
@@ -249,7 +299,11 @@ const Savanna = () => {
   }, [wordNumber, words]);
 
   useEffect(() => {
-    dispatch(getSavannaInfo());
+    if (isUserLoggedIn) {
+      getUserWordsWrapper();
+    } else {
+      dispatch(getSavannaInfo(false));
+    }
   }, []);
 
   useEffect(() => {
